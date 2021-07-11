ef138177bf39cba074e7e4ed656197bb9d4502bf
Luka Bozanovic <lukabozanovic@gmail.com>
2021-07-11 21:10:01 +0200

    Initial Commit

---
+++ Shared/TwitterSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUICloneApp.swift
+//  Shared
+//
+//  Created by Luka Bozanovic on 11.07.21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
