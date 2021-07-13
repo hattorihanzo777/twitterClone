e573c82ae81bd3dcf01195db9c80ece530d22152
Luka Bozanovic <lukabozanovic@gmail.com>
2021-07-13 23:20:04 +0200

    First

---
+++ View/Messages/NewMessageView.swift
@@ -0,0 +1,20 @@
+//
+//  NewMessageView.swift
+//  TwitterSwiftUIClone (iOS)
+//
+//  Created by Luka Bozanovic on 13.07.21.
+//
+
+import SwiftUI
+
+struct NewMessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewMessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewMessageView()
+    }
+}
